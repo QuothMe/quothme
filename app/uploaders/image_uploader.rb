b1690a0840e6f7a84ecdd5d31f42ab11dbcf6e82
Sara Ahbabou <ahbabousara@gmail.com>
2016-10-26 13:59:08 -0700

    syntax

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,7 +15,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     config.storage = :s3
   end
 end
-    end
+    
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
