83fe12e3c78b2679095b8b9c07c5ddca880498bc
Sara Ahbabou <ahbabousara@gmail.com>
2016-10-26 22:18:16 -0700

    syntax

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,8 @@
 CarrierWave.configure do |config|
-
-   if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-    else
-      config.storage =:aws
-    config.aws_credentials = {
-      :provider               => 'AWS',                        # required
-      :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
-      :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
-    }
-
-
-  config.aws_directory  = ENV["AWS_BUCKET"]                     # required
-  end
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
+  }
+  config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
