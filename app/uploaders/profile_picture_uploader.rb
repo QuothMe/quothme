27edb6ad02ccf3ba0b9b57a48389e3e6ed158e67
Sara Ahbabou <ahbabousara@gmail.com>
2016-10-26 21:51:40 -0700

    syntax

--- app/uploaders/profile_picture_uploader.rb
+++ app/uploaders/profile_picture_uploader.rb
@@ -6,6 +6,11 @@ class ProfilePictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
+  if Rails.env.development? || Rails.env.test?
+      storage = :file
+    else
+      storage = :fog
+    end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
