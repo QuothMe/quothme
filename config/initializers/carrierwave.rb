5d9bb19cc20644940f3a3859a561e48f4e3764b1
Sara Ahbabou <ahbabousara@gmail.com>
2016-10-26 22:26:10 -0700

    syntax

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
   config.aws_bucket = ENV["AWS_BUCKET"] 
 
   config.aws_credentials = {
-    :provider               => 'AWS',                        # required
+                           # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
     :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
   }
