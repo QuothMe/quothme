f78d5d7671616a5b756af3d688e0f49afa9331af
Sara Ahbabou <ahbabousara@gmail.com>
2016-11-03 20:20:53 -0700

    more rspec

--- spec/controllers/quotes_controller_spec.rb
+++ spec/controllers/quotes_controller_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe QuotesController, type: :controller do
       it 'Should let users create quotes' do
 
       user = FactoryGirl.create(:user)
-      quote = FactoryGirl.create(:quote,tag: 'tag')
+      quote = FactoryGirl.create(:quote)
     
       sign_in user
-      post :create, quote_id: quote.id, tag_id: tag.id, quote: {citation: 'And then they fell'}
+      post :create, tag: 'tag', quote_id: quote.id, quote: {citation: 'And then they fell'}
       expect(response).to redirect_to root_path
     end
   end
