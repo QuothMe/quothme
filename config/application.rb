da5e627b364b6c42877c88b2ae509dbfe17db046
Sara Ahbabou <sara@Saras-MacBook-Pro.local>
2016-11-25 17:14:47 -0800

    .

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+    config.assets.initialize_on_precompile = false
 
 
 
 module Quothme
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
     config.action_view.embed_authenticity_token_in_remote_forms = true
 
     
