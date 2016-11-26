d5ed988386f5b39c157100881f213a703a47ece6
Sara Ahbabou <sara@Saras-MacBook-Pro.local>
2016-11-25 18:08:47 -0800

    ,

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,10 @@ Bundler.require(*Rails.groups)
 module Quothme
   class Application < Rails::Application
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.assets.initialize_on_precompile = false
 
+    
+    
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
