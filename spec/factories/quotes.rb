5d10f51b6a81a9553c136cd52eb64acc0c68b183
Sara Ahbabou <sara@Saras-MacBook-Pro.local>
2017-01-07 19:12:07 -0800

    travis errors

--- spec/factories/quotes.rb
+++ spec/factories/quotes.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
 	factory :quote do
 		citation 'She said things'
 		tag_list 'hello'
-		image Rack::Test::UploadedFile.new(Rails.root + 'spec/fixtures/buscemi.png', 'image/jpg')
+		
 		user 
 	end
 
